a3a3636409d6edc3d5281d324d99a4a30055b7a2
dim web <dmitrymail_k@mail.ru>
2022-01-19 23:47:15 +0300

    milkdown basic setup

---
+++ sandbox/markdown_editor/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <MilkdownEditor />
+  </div>
+</template>
+
+<script>
+import { MilkdownEditor } from "./components/MilkdownEditor";
+export default {
+  name: "App",
+  components: {
+    MilkdownEditor,
+  },
+};
+</script>
+
+<style>
+#app {
+  height: 100vh;
+  width: 100%;
+}
+</style>
