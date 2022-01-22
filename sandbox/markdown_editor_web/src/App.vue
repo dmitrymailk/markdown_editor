cc4aea4c822371f5455004a5e02680125691b575
dim web <dmitrymail_k@mail.ru>
2022-01-22 19:21:31 +0300

    Revert "delete useless files"
    This reverts commit cdcf930fb0f8b6636e6d214ec89edb20692acad0.

--- sandbox/markdown_editor_web/src/App.vue
+++ sandbox/markdown_editor_web/src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <div>
+    <!-- <MilkdownEditor /> -->
     <ExampleEditor />
   </div>
 </template>
 
 <script>
+// import { MilkdownEditor } from "./components/MilkdownEditor";
 import ExampleEditor from "./components/official_examples/ExampleEditor.vue";
 export default {
   name: "App",
   components: {
+    // MilkdownEditor,
     ExampleEditor,
   },
 };
