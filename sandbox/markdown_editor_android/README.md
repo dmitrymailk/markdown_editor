cd618a0ef51f2863cd806df90e3f0ca85daf4253
dim web <dmitrymail_k@mail.ru>
2022-01-30 17:21:51 +0300

    setup working capacitor filesystem

--- sandbox/markdown_editor_android/README.md
+++ sandbox/markdown_editor_android/README.md
@@ -7,3 +7,18 @@
 3.  ionic serve
 
 - [BUG! exception in phase 'semantic analysis' in source unit '](https://stackoverflow.com/questions/68597899/bug-exception-in-phase-semantic-analysis-in-source-unit-buildscript-unsup) - сперва лучше проверить sdk в android studio и довериться ему в автоматическом разрешении проблем
+
+## Start develop on android
+
+- ionic capacitor copy android
+- npx cap open android
+
+## Start Live reload
+
+- ionic capacitor run android -l --external
+- chrome://inspect
+
+## Links
+
+- [Ionic capasitor filesystem](https://capacitorjs.com/docs/apis/filesystem)
+- [Ionic live reload](https://ionicframework.com/docs/cli/livereload)
--- sandbox/markdown_editor_android/src/components/milkdown-editor/ExampleEditor.vue
+++ sandbox/markdown_editor_android/src/components/milkdown-editor/ExampleEditor.vue
@@ -5,6 +5,8 @@
 <script>
 import { MyEditor } from "./VueEditor";
 import "material-icons/iconfont/material-icons.css";
+import { Filesystem, Directory, Encoding } from "@capacitor/filesystem";
+
 const markdown = ``;
 export default {
   data() {
@@ -15,16 +17,65 @@ export default {
   components: {
     MyEditor,
   },
+  methods: {
+    async writeSecretFile() {
+      await Filesystem.writeFile({
+        path: "secrets/text.txt",
+        data: "This is a test",
+        directory: Directory.Documents,
+        encoding: Encoding.UTF8,
+      });
+    },
+    async readSecretFile() {
+      const contents = await Filesystem.readFile({
+        path: "secrets/text.txt",
+        directory: Directory.Documents,
+        encoding: Encoding.UTF8,
+      });
+
+      console.log("secrets:", contents);
+    },
+    async deleteSecretFile() {
+      await Filesystem.deleteFile({
+        path: "secrets/text.txt",
+        directory: Directory.Documents,
+      });
+    },
+    async readFilePath() {
+      // Here's an example of reading a file with a full file path. Use this to
+      // read binary data (base64 encoded) from plugins that return File URIs, such as
+      // the Camera.
+      const contents = await Filesystem.readFile({
+        path: "file:///var/mobile/Containers/Data/Application/22A433FD-D82D-4989-8BE6-9FC49DEA20BB/Documents/text.txt",
+      });
+
+      console.log("data:", contents);
+    },
+  },
+  async mounted() {
+    let permission = await Filesystem.checkPermissions();
+    if (permission.publicStorage !== "granted") {
+      permission = await Filesystem.requestPermissions();
+      console.log("request permission", permission);
+    } else {
+      console.log("permission ok");
+      const APP_DIR = Directory.ExternalStorage;
+      console.log(APP_DIR);
+      try {
+        const dir = await Filesystem.readdir({
+          path: "/",
+          directory: APP_DIR,
+        });
+        console.log(dir);
+      } catch (error) {
+        console.log("mounted", error);
+      }
+    }
+  },
 };
 </script>
 
 <style lang="sass">
-// body, #app, html, #app-container
-//     height: 300px
-//     width: 100%
-//     margin: 0
-//     overflow: hidden
-//     touch-action: none
 
 .drawing
     display: flex
